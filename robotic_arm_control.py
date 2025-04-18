41d63478a7612222a56cb2a8b8c0ae123a8b71f7
brian2009VV <144502111+brian2009VV@users.noreply.github.com>
2025-04-18 17:04:59 +0800

    Update robotic_arm_control.py

--- robotic_arm_control.py
+++ robotic_arm_control.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import time
 import math
 import numpy as np
 
@@ -29,7 +30,11 @@ class RoboticController:
         if goal_position_x ** 2 + goal_position_y ** 2 == 0:
             alpha =  math.pi / 2 - math.atan(L2/L1) - math.acos(t) - math.pi / 2
         else:
-            alpha = math.pi / 2 - math.atan(L2/L1) - math.acos(t) - math.atan(goal_position_z/math.sqrt(goal_position_x ** 2 + goal_position_y ** 2))
+            if goal_position_y >= 0:
+                alpha = math.pi / 2 - math.atan(L2 / L1) - math.acos(t) - math.atan(goal_position_z / math.sqrt(goal_position_x ** 2 + goal_position_y ** 2))
+            else:
+                alpha = math.pi / 2 - math.atan(L2 / L1) - math.acos(t) - (np.pi - math.atan(goal_position_z / math.sqrt(goal_position_x ** 2 + goal_position_y ** 2)))
+
 
         xa = L1 * math.sin(alpha) * math.sin(sita)
         ya = L1 * math.sin(alpha) * math.cos(sita)
@@ -40,7 +45,7 @@ class RoboticController:
         zb = za - L2 * math.sin(alpha)
 
         k = (xa - xb) * (goal_position_x - xb) + (ya - yb) * (goal_position_y - yb) + (za - zb) * (goal_position_z - zb)
-
+        print(k / (L2 * L3))
         beta = math.pi - math.acos(k / (L2 * L3))
 
         m = xb - xa
@@ -100,7 +105,8 @@ class RoboticController:
             f = f
         return f / np.pi * 180
 
-    def get_to_real(self, goal_x, goal_y, goal_z, horizontal_angle):
+    def calulate_to_xyza(self, goal_x, goal_y, goal_z, horizontal_angle):
+        horizontal_angle = horizontal_angle / 180 * np.pi
         p = self.calculate_relative_coordinate(goal_x, goal_y, goal_z, horizontal_angle)
         print(p[0], p[1], p[2])
         l = self.calculate_angle(p[0], p[1], p[2])
@@ -113,3 +119,87 @@ class RoboticController:
 
         res = [l[0], l[1], l[2], a]
         return res
+
+    def open_close_clip(self, id, alpha, Dy):
+        angle_max = 90
+        angle_min = -8
+        if alpha > 90 or alpha < -8:
+            return None
+
+        Dy.goal_absolute_direction(id, alpha)
+
+    def turn_clip(self, id, alpha, Dy):
+        if alpha < -90 or alpha > 90:
+            return None
+
+        Dy.goal_absolute_direction(id, alpha)
+
+    def calculate_vel(self, ang, id_list, Dy):
+        p = []
+        for i in id_list:
+            if i == 15:
+                continue
+            p.append(Dy.present_position(i))
+
+        m = 0
+        for i in range(len(p)):
+            m = max(m, abs(ang[i] - p[i]))
+
+        vel_percent = []
+        for i in range(len((p))):
+            vel_percent.append(abs(ang[i] - p[i]) / m)
+
+        velocity = []
+        v_max = 20
+        for i in range(len(vel_percent)):
+            k = int(v_max * vel_percent[i])
+
+            if k == 0:
+                k = 1
+
+            velocity.append(k)
+
+        return velocity
+
+    def open_robotic_arm(self, com, id_list, Dy):
+        Dy.open(com)
+        for i in id_list:
+            Dy.torque_enable(i)
+
+    def go_to_real_xyz_alpha(self, id_list, goal_position_list, horizontal_angle, turn_clip_angle, open_clip_angle, state, Dy):
+        if state == 0:
+            turn_clip_angle = -turn_clip_angle
+
+        px = goal_position_list[0]
+        py = goal_position_list[1]
+        pz = goal_position_list[2]
+
+        data = self.calulate_to_xyza(px, py, pz, horizontal_angle)
+
+        if data is None:
+            return None
+
+        ang = [data[0], data[1], data[2], data[3], turn_clip_angle, open_clip_angle]
+
+        vel = self.calculate_vel(ang, id_list, Dy)
+
+        print("ang = ", ang)
+        print("vel = ", vel)
+
+        Dy.profile_velocity(id_list[0], vel[0])
+        Dy.profile_velocity(id_list[1], vel[1])
+        Dy.profile_velocity(id_list[2], vel[1])
+        Dy.profile_velocity(id_list[3], vel[2])
+        Dy.profile_velocity(id_list[4], vel[3])
+        Dy.profile_velocity(id_list[5], vel[4])
+        Dy.profile_velocity(id_list[6], vel[5])
+
+        Dy.goal_absolute_direction(id_list[0], ang[0])
+        Dy.goal_absolute_direction(id_list[1], ang[1])
+        Dy.goal_absolute_direction(id_list[2], -ang[1])
+        Dy.goal_absolute_direction(id_list[3], ang[2])
+        Dy.goal_absolute_direction(id_list[4], ang[3])
+        self.turn_clip(id_list[5], ang[4], Dy)
+        self.open_close_clip(id_list[6], ang[5], Dy)
+        time.sleep(5)
+
